d5ebf10774d720074653eb0d391395bfd3d14095
macbook air <macbookair@MacBook-Air--macbook.local>
2026-03-17 22:36:01 +0500

    thd commit

--- js/script.js
+++
@@ -1,8 +0,0 @@
-const navToggle = document.querySelector(".nav-toggle");
-const ul = document.querySelector("header nav ul");
-
-navToggle.addEventListener("click", () => {
-  navToggle.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
-
-  ul.classList.toggle("active")
-});
